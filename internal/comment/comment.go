67c488513c0e6ab967b02f08ee7f1361e4502716
Himanshu <kandpallucky@gmail.com>
2024-02-19 19:00:40 +0530

    Added basic structure of other CRUD features

--- internal/comment/comment.go
+++ internal/comment/comment.go
@@ -3,17 +3,25 @@ package comment
 import (
 	"fmt"
 	"context"
+	"errors"
 )
 
-type Store interface {
-	GetComment(context.Context, string) (Comment, error) 
-}
+var (
+	ErrFetchComment = errors.New("failed to fetch comment by id.")
+	ErrNotImplemented = errors.New("Not Implemented") 
+)
 
 type Comment struct {
 	ID string
 	Slug string
 	Body string
 	Author string
+}
+
+// Store - defines all the methods
+// that our service needs in order to operate
+type Store interface {
+	GetComment(context.Context, string) (Comment, error) 
 }
 
 // Service - all our logic resides
@@ -35,7 +43,19 @@ func (s *Service) GetComment(ctx context.Context, id string) (Comment, error) {
 	cmt, err := s.Store.GetComment(COmment, id)
 	if err!=nil {
 		fmt.Println(err)
-		return Comment{}, err
+		return Comment{}, ErrFetchComment
 	}
 	return cmt, nil
+}
+
+func (s *Service) UpdateComment(ctx context.Context, cmt Comment) error {
+	return ErrNotImplemented
+}
+
+func (s *Service) DeleteComment(ctx context.Context, id string) error {
+	return ErrNotImplemented
+}
+
+func (s *Service) CreateComment(ctx context.Context, cmt Comment) (Comment, error) {
+	return Comment{}, ErrNotImplemented
 }
